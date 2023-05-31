9d2a18444566105244cc1d3ae0e835c4505432fa
George Kaye <georgejkaye@gmail.com>
2023-05-31 22:30:32 +0100

    [feature] Add more functions for getting data from bustimes pages

--- src/scrapers.py
+++ src/scrapers.py
@@ -1,7 +1,10 @@
-from typing import Optional, Tuple
+import re
+from typing import List, Optional, Tuple
 import requests
-from bs4 import BeautifulSoup  # type: ignore
-from datetime import datetime, time
+from bs4 import BeautifulSoup, Tag  # type: ignore
+from datetime import date, datetime, time
+
+from structs import BusStop, BusTrip, BusTripSegment, BusTripStop
 
 
 def get_page(url: str) -> BeautifulSoup:
@@ -29,8 +32,46 @@ def get_bus_stop_page(id: int, origin_dt: datetime) -> BeautifulSoup:
 
 
 def get_bus_stop_name(stop_page: BeautifulSoup) -> str:
-    results = stop_page.select_one("h1")
-    return results.text
+    result = stop_page.select_one("h1")
+    return result.text
+
+
+def get_bus_stop_latlon(stop_page: BeautifulSoup) -> Tuple[float, float]:
+    result = stop_page.select_one(".horizontal a")
+    link = result["href"]
+    split = link.split("/")
+    lat = split[2]
+    lon = split[3]
+    return (float(lat), float(lon))
+
+
+def get_bus_stop_naptan(stop_page: BeautifulSoup) -> str:
+    result = stop_page.select_one("[title=\"NaPTAN code\"]")
+    return result.text
+
+
+def get_bus_stop_atco(stop_page: BeautifulSoup) -> int:
+    result = stop_page.select_one("[title=\"ATCO code\"]")
+    return int(result.text)
+
+
+bracket_regex = r"(.*) \((.*)\)"
+
+
+def get_bus_stop(id: int) -> BusStop:
+    stop_url = get_bus_stop_url(id)
+    page = get_page(stop_url)
+    stop_name = get_bus_stop_name(page)
+    bracket_matches = re.match(bracket_regex, stop_name)
+    if bracket_matches is not None:
+        stop_name = bracket_matches.group(1)
+        stop_stand = bracket_matches.group(2)
+    else:
+        stop_stand = None
+    (stop_lat, stop_lon) = get_bus_stop_latlon(page)
+    stop_naptan = get_bus_stop_naptan(page)
+    stop_atco = get_bus_stop_atco(page)
+    return BusStop(stop_name, stop_stand, stop_lat, stop_lon, stop_naptan, stop_atco)
 
 
 def get_bus_trip_page(id: int, stop_time: Optional[int] = None) -> BeautifulSoup:
@@ -39,18 +80,44 @@ def get_bus_trip_page(id: int, stop_time: Optional[int] = None) -> BeautifulSoup
 
 
 def get_bus_number(trip_page: BeautifulSoup) -> str:
-    results = trip_page.select_one("h2").text.strip().replace("\n", "")
-    return results.split("-")[0]
+    result = trip_page.select_one("h2").text.strip().replace("\n", "")
+    return result.split("-")[0]
 
 
-def get_stop_details_from_trip(trip_page: BeautifulSoup, stop_time: int) -> Tuple[str, int, time]:
-    row = trip_page.select_one("tr", id=f"stop-time-{stop_time}")
+def get_trip_origin_and_destination(trip_page: BeautifulSoup) -> Tuple[str, str]:
+    result = trip_page.select_one("h2").text.replace("\n", "")
+    first_hyphen = result.find("-")
+    route = result[first_hyphen+1:]
+    route_split = route.split(" to ")
+    origin = route_split[0]
+    destination = route_split[1]
+    return (origin, destination)
+
+
+def get_bus_service_number_and_slug(trip_page: BeautifulSoup) -> Tuple[str, str]:
+    result = trip_page.select_one("ol li:nth-child(3) a")
+    slug = result["href"].split("/")[2]
+    number = slug.split("-")[0]
+    return (number, slug)
+
+
+def get_trip_stop_details(row: Tag) -> BusTripStop:
     link = row.select_one("a")
-    stop_id = link["href"].split("/")[2]
-    stop = link.text
+    stop_id = int(link["href"].split("/")[2])
+    stop_name = link.text
     (hours, minutes) = row.select("td")[1].text.strip().split(":")
     time_obj = time(hour=int(hours), minute=int(minutes))
-    return (stop, stop_id, time_obj)
+    return BusTripStop(stop_name, stop_id, time_obj)
+
+
+def get_trip_stop_details_at_time(trip_page: BeautifulSoup, stop_time: int) -> BusTripStop:
+    row = trip_page.select_one(f"[id=\"stop-time-{stop_time}\"")
+    return get_trip_stop_details(row)
+
+
+def get_all_trip_stop_details(trip_page: BeautifulSoup) -> List[BusTripStop]:
+    rows = trip_page.select("[id*=\"stop-time\"]")
+    return list(map(lambda r: get_trip_stop_details(r), rows))
 
 
 def get_stop_time_from_stop_id(trip_page: BeautifulSoup, stop_id: int) -> Optional[int]:
@@ -60,4 +127,30 @@ def get_stop_time_from_stop_id(trip_page: BeautifulSoup, stop_id: int) -> Option
         candidate_stop_id = link["href"].split("/")[2]
         if int(candidate_stop_id) == stop_id:
             return row["id"].split("-")[2]
+    return None
+
+
+def get_bus_trip(id: int) -> BusTrip:
+    page = get_bus_trip_page(id)
+    (trip_number, trip_slug) = get_bus_service_number_and_slug(page)
+    (origin, destination) = get_trip_origin_and_destination(page)
+    stops = get_all_trip_stop_details(page)
+    return BusTrip(id, trip_number, trip_slug, origin, destination, stops[0].time, stops)
+
+
+def get_bus_trip_segment(trip: BusTrip, board: int, alight: int) -> Optional[BusTripSegment]:
+    start = None
+    end = None
+    for (i, stop) in enumerate(trip.stops):
+        if stop.atco == board:
+            start = i
+        elif stop.atco == alight:
+            if start is None:
+                return None
+            end = i
+            start_time = trip.stops[start].time
+            end_time = trip.stops[end].time
+            duration = datetime.combine(
+                date.min, end_time) - datetime.combine(date.min, start_time)
+            return BusTripSegment(trip, start, end, duration)
     return None
--- src/structs.py
+++ src/structs.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from datetime import time
+from datetime import time, timedelta
 from typing import List, Optional
 
 
@@ -31,13 +31,45 @@ class BusService:
 
 @dataclass
 class BusTripStop:
-    stop: BusStop
+    name: str
+    atco: int
     time: time
 
 
 @dataclass
 class BusTrip:
-    service: BusService
     id: int
+    service: str
+    service_slug: str
+    origin: str
+    destination: str
     start_time: time
     stops: List[BusTripStop]
+
+
+def get_duration_string(d: timedelta) -> str:
+    seconds = d.seconds
+    hours = int(seconds / 3600)
+    minutes = int(seconds / 60)
+    if hours > 0:
+        hour_string = f"{hours}h"
+    else:
+        hour_string = ""
+    return f"{hour_string}{minutes}m"
+
+
+@dataclass
+class BusTripSegment:
+    trip: BusTrip
+    board: int
+    alight: int
+    duration: timedelta
+
+    def get_segment_stops(self) -> List[BusTripStop]:
+        return self.trip.stops[self.board:self.alight+1]
+
+    def get_boarding_stop(self) -> BusTripStop:
+        return self.trip.stops[self.board]
+
+    def get_alighting_stop(self) -> BusTripStop:
+        return self.trip.stops[self.alight]
