932da7b40c4c49e95b27d00ad1123639142306bb
George Kaye <georgejkaye@gmail.com>
2023-06-16 00:14:12 +0100

    [impl] Update train structs

--- src/train/structs.py
+++ src/train/structs.py
@@ -1,12 +1,16 @@
 from dataclasses import dataclass
+from datetime import datetime
+from dataclasses_json import dataclass_json
 
-from typing import Optional
+from typing import List, Optional
 
 
+@dataclass_json
 @dataclass
 class TrainStation:
     name: str
     crs: str
+    tiploc: str
     lat: Optional[float]
     lon: Optional[float]
 
@@ -15,3 +19,10 @@ class TrainStation:
 class TrainStop:
     station: TrainStation
     platform: Optional[str]
+
+
+@dataclass
+class TrainService:
+    headcode: str
+    origins: List[str]
+    date: datetime
