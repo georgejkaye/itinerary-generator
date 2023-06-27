0557a14176657b628f0bc667bab0d3f8f4ee778b
George Kaye <georgejkaye@gmail.com>
2023-06-27 23:47:16 +0100

    [feature] Add train input parser

--- src/input.py
+++ src/input.py
@@ -1,9 +1,9 @@
 from typing import Dict
 
 import arrow
-from bus.scrapers import get_bus_trip, get_bus_trip_segment
+from bus.scrapers import get_bus_trip
 from credentials import Credentials
-from structs import Segment
+from structs import Segment, get_segment
 from train.scrapers import make_train_service
 from train.structs import TrainService, TrainStation
 
@@ -14,17 +14,21 @@ def parse_bus_element(item: dict) -> Segment:
     board = item["board"]
     alight = item["alight"]
     trip = get_bus_trip(date, id)
-    segment = get_bus_trip_segment(trip, board, alight)
+    segment = get_segment(trip, board, alight)
     if segment is None:
         raise RuntimeError("Not a valid segment")
     return segment
 
 
-def read_train_element(
+def parse_train_element(
     element: dict, rtt_credentials: Credentials, crs_lookup: Dict[str, TrainStation]
-) -> TrainService:
+) -> Segment:
     id = element["id"]
-    date = element["date"]
-    origin = element["origin"]
-    destination = element["destination"]
-    return make_train_service(id, date, rtt_credentials, crs_lookup)
+    date = arrow.get(element["date"])
+    board = element["board"]
+    alight = element["alight"]
+    trip = make_train_service(id, date, rtt_credentials, crs_lookup)
+    segment = get_segment(trip, board, alight)
+    if segment is None:
+        raise RuntimeError("Not a valid segment")
+    return segment
