28a2eeb0e10c3f1d604fafc70a4e98ab5b17538a
George Kaye <georgejkaye@gmail.com>
2023-07-08 18:03:53 +0100

    [impl] Improve how generated itineraries look

--- src/output.py
+++ src/output.py
@@ -1,10 +1,15 @@
 from datetime import timedelta
+import os
 from pathlib import Path
+import shutil
 from typing import List
 from arrow import Arrow
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 from structs import Segment, get_duration_string
+
+assets_dir = Path("assets")
+css_path = assets_dir / "styles.css"
 
 
 def ampersand(input: List[str]):
@@ -23,7 +28,7 @@ def hours_minutes(input: timedelta):
     return get_duration_string(input)
 
 
-def write_output(segments: List[Segment], output_file: Path | str):
+def write_output(segments: List[Segment], output_path: Path | str):
     env = Environment(
         loader=FileSystemLoader("templates"), autoescape=select_autoescape()
     )
@@ -36,5 +41,9 @@ def write_output(segments: List[Segment], output_file: Path | str):
     }
     index = env.get_template("index.html")
     html = index.render(segments=segments)
-    with open(output_file, "w") as file:
+    html_path = Path(output_path) / "index.html"
+    os.makedirs(output_path, exist_ok=True)
+    with open(html_path, "w") as file:
         file.write(html)
+    output_css_path = Path(output_path) / "styles.css"
+    shutil.copyfile(css_path, output_css_path)
