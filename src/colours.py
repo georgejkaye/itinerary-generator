c44210d5795d923d01b5296b0d1ae3477990befe
George Kaye <georgejkaye@gmail.com>
2023-07-16 20:11:58 +0100

    [refact] Generalise colour acquisition even more

--- src/colours.py
+++ src/colours.py
@@ -1,15 +1,16 @@
 import os
 
-from typing import Any, Optional
+from typing import Any, Callable, Optional, TypeVar
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 from bus.scrapers import get_bus_stop
-from bus.structs import BusTrip
+from bus.structs import BusTrip, BusTripStop
 
 from data import colours_dir, record_colours
+from structs import TripStopInterface
 
 
 def get_service_colour(slug: str) -> Optional[tuple[str, str]]:
@@ -64,18 +65,31 @@ def get_style_dict(style: str) -> dict[str, str]:
     return style_dict
 
 
-def get_bus_trip_colour(trip: BusTrip, driver) -> tuple[str, str]:
-    possible_colour = get_service_colour(trip.slug)
-    if possible_colour is not None:
-        return possible_colour
+def check_for_route(driver, route: str) -> Optional[Any]:
+    elements = driver.find_elements(By.CLASS_NAME, "Bzv5Cd")
+    for elem in elements:
+        if elem.text.startswith(route):
+            return elem
+    return None
+
+
+T = TypeVar("T")
+
+
+def get_segment_colour(
+    route: str,
+    route_identifier: str,
+    stops: list[T],
+    get_search_string: Callable[[T], str],
+    driver,
+) -> tuple[str, str]:
     bg_colour = None
     fg_colour = None
     try:
         current_stop_index = 0
         while bg_colour is None and fg_colour is None:
-            current_stop_atco = trip.stops[current_stop_index].stop.atco
-            current_stop = get_bus_stop(current_stop_atco)
-            search_string = f"bus stop {current_stop.name} {current_stop.indicator} {current_stop.street} {current_stop.parent}"
+            current_stop = stops[current_stop_index]
+            search_string = get_search_string(current_stop)
             search_url = f"https://www.google.co.uk/maps/?q={search_string}"
             make_selenium_request(driver, search_url)
             element = wait_for_selector(driver, 30, ".cb7kab, .cukLmd")
@@ -83,18 +97,30 @@ def get_bus_trip_colour(trip: BusTrip, driver) -> tuple[str, str]:
                 print("opened cookies")
                 close_google_cookies_screen(driver)
             print("closed cookies")
-            elements = driver.find_elements(By.CLASS_NAME, "Bzv5Cd")
-            for elem in elements:
-                if elem.text.startswith(trip.number):
-                    style = elem.get_attribute("style")
-                    style_dict = get_style_dict(style)
-                    bg_colour = get_colour(style_dict, "background-color", "#808080")
-                    fg_colour = get_colour(style_dict, "color", "#ffffff")
-                    break
+            route_box = check_for_route(driver, route)
+            if route_box is not None:
+                style = route_box.get_attribute("style")
+                style_dict = get_style_dict(style)
+                bg_colour = get_colour(style_dict, "background-color", "#808080")
+                fg_colour = get_colour(style_dict, "color", "#ffffff")
+                break
             current_stop_index = current_stop_index + 1
     except:
         print("Could not get colour!")
         fg_colour = input("Background colour: ")
         bg_colour = input("Foreground colour: ")
-    record_colours(trip.slug, bg_colour, fg_colour)
+    record_colours(route_identifier, bg_colour, fg_colour)
     return (bg_colour, fg_colour)
+
+
+def get_bus_stop_search_string(stop: BusTripStop) -> str:
+    return f"bus stop {stop.stop.name} {stop.stop.indicator} {stop.stop.street} {stop.stop.parent}"
+
+
+def get_bus_trip_colour(trip: BusTrip, driver) -> tuple[str, str]:
+    possible_colour = get_service_colour(trip.slug)
+    if possible_colour is not None:
+        return possible_colour
+    return get_segment_colour(
+        trip.number, trip.slug, trip.stops, get_bus_stop_search_string, driver
+    )
