b4784cabb85704d3192ffad51e6825842db4e2ff
George Kaye <georgejkaye@gmail.com>
2023-07-23 20:54:01 +0100

    [impl] Update what structs return in api

--- api/src/main.py
+++ api/src/main.py
@@ -1,9 +1,17 @@
-from fastapi import FastAPI
+from http.client import HTTPException
+from typing import Optional
+from fastapi import FastAPI, HTTPException
 
 from database.methods import connect, disconnect
 
 from structs.bus import BusStop, BusRoute, select_bus_route, select_bus_stop
-from structs.train import TocWithColours, select_toc
+from structs.train import (
+    TocData,
+    TrainStation,
+    TrainStationData,
+    select_toc,
+    select_train_station,
+)
 
 app = FastAPI()
 
@@ -13,6 +21,8 @@ async def get_stop(atco: str):
     (conn, cur) = connect()
     stop = select_bus_stop(cur, atco)
     disconnect(conn, cur)
+    if stop is None:
+        raise HTTPException(status_code=404, detail=f"Atco {atco} not found")
     return stop
 
 
@@ -21,12 +31,28 @@ async def get_route(slug: str):
     (conn, cur) = connect()
     route = select_bus_route(cur, slug)
     disconnect(conn, cur)
+    if route is None:
+        raise HTTPException(status_code=404, detail=f"Slug {slug} not found")
     return route
 
 
-@app.get("/train/toc/{atoc}", response_model=TocWithColours)
+@app.get("/train/station/{descriptor}", response_model=TrainStationData)
+async def get_station(descriptor: str):
+    (conn, cur) = connect()
+    station = select_train_station(cur, descriptor)
+    disconnect(conn, cur)
+    if station is None:
+        raise HTTPException(
+            status_code=404, detail=f"Station with descriptor {descriptor} not found"
+        )
+    return station
+
+
+@app.get("/train/toc/{atoc}", response_model=TocData)
 async def get_toc(atoc: str):
     (conn, cur) = connect()
     toc = select_toc(cur, atoc)
     disconnect(conn, cur)
+    if toc is None:
+        raise HTTPException(status_code=404, detail=f"Toc with atoc {atoc} not found")
     return toc
--- api/src/structs/train.py
+++ api/src/structs/train.py
@@ -1,9 +1,11 @@
+from re import S
+from typing import Optional
 import xml.etree.ElementTree as ET
 
 from dataclasses import dataclass
 from database.methods import select
 
-from pull.core import get_tag_text, make_request, prefix_namespace
+from pull.core import get_or_throw, get_tag_text, make_request, prefix_namespace
 from pull.train import (
     get_kb_url,
     get_natrail_token_headers,
@@ -12,6 +14,69 @@ from pull.train import (
 )
 
 from database.schema import *
+
+
+@dataclass
+class Toc:
+    name: str
+    atoc: str
+
+
+def pull_tocs(natrail_token: str) -> list[Toc]:
+    kb_tocs_url = get_kb_url("tocs")
+    headers = get_natrail_token_headers(natrail_token)
+    kb_tocs = make_request(kb_tocs_url, headers=headers).text
+    kb_tocs_xml = ET.fromstring(kb_tocs)
+    tocs = []
+    for toc in kb_tocs_xml.findall(
+        prefix_namespace(kb_tocs_namespace, "TrainOperatingCompany")
+    ):
+        toc_name = get_tag_text(toc, "Name", kb_tocs_namespace)
+        toc_code = get_tag_text(toc, "AtocCode", kb_tocs_namespace)
+        tocs.append(Toc(toc_name, toc_code))
+    return tocs
+
+
+@dataclass
+class Brand:
+    atoc: str
+    endpoints: list[str]
+
+
+@dataclass
+class TocData:
+    name: str
+    atoc: str
+    fg: str
+    bg: str
+    brands: list[Brand]
+
+
+def select_brands(cur, atoc: str) -> list[Brand]:
+    rows = select(
+        cur,
+        ["atoc", "endpoints"],
+        brand_table,
+        ["parent = %(parent)s"],
+        {"parent": atoc},
+    )
+    return list(map(lambda row: Brand(row[0], row[1]), rows))
+
+
+def select_toc(cur, atoc: str) -> Optional[TocData]:
+    rows = select(
+        cur,
+        ["toc.name", "toc.atoc", "colour.fg", "colour.bg"],
+        f"{toc_table} LEFT OUTER JOIN {colour_table} ON toc.atoc = colour.code",
+        ["atoc = %(atoc)s"],
+        {"atoc": atoc},
+    )
+    if len(rows) != 1:
+        return None
+    else:
+        row = rows[0]
+        brands = select_brands(cur, atoc)
+        return TocData(row[0], row[1], row[2], row[3], brands)
 
 
 @dataclass
@@ -45,44 +110,25 @@ def pull_stations(natrail_token: str) -> list[TrainStation]:
 
 
 @dataclass
-class Toc:
+class TrainStationData:
     name: str
-    atoc: str
+    crs: str
+    lat: float
+    lon: float
+    operator: Toc
 
 
-def pull_tocs(natrail_token: str) -> list[Toc]:
-    kb_tocs_url = get_kb_url("tocs")
-    headers = get_natrail_token_headers(natrail_token)
-    kb_tocs = make_request(kb_tocs_url, headers=headers).text
-    kb_tocs_xml = ET.fromstring(kb_tocs)
-    tocs = []
-    for toc in kb_tocs_xml.findall(
-        prefix_namespace(kb_tocs_namespace, "TrainOperatingCompany")
-    ):
-        toc_name = get_tag_text(toc, "Name", kb_tocs_namespace)
-        toc_code = get_tag_text(toc, "AtocCode", kb_tocs_namespace)
-        tocs.append(Toc(toc_name, toc_code))
-    return tocs
-
-
-@dataclass
-class TocWithColours:
-    name: str
-    atoc: str
-    fg: str
-    bg: str
-
-
-def select_toc(cur, atoc: str) -> TocWithColours:
+def select_train_station(cur, descriptor: str) -> Optional[TrainStationData]:
     rows = select(
         cur,
-        ["toc.name", "toc.atoc", "colour.fg", "colour.bg"],
-        f"{toc_table} INNER JOIN {colour_table} ON toc.atoc = colour.code",
-        ["atoc = %(atoc)s"],
-        {"atoc": atoc},
+        ["crs", "name", "lat", "lon", "operator"],
+        train_station_table,
+        ["crs = %(desc)s OR name = %(desc)s"],
+        {"desc": descriptor},
     )
     if len(rows) != 1:
-        raise RuntimeError(f"No match for atoc {atoc}")
+        return None
     else:
         row = rows[0]
-        return TocWithColours(row[0], row[1], row[2], row[3])
+        operator = get_or_throw(select_toc(cur, row[4]))
+        return TrainStationData(row[0], row[1], row[2], row[3], operator)
