3ed45a8ea5ae5da3bd332a82d94e272142ff58ae
George Kaye <georgejkaye@gmail.com>
2023-07-23 23:19:19 +0100

    [impl] Move some colours logic to api

--- gen/src/colours.py
+++ gen/src/colours.py
@@ -6,18 +6,15 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-from data import colours_dir, record_colours
-
 from bus.structs import BusTrip, BusTripStop
+from request import get_json_from_api
 from train.structs import TrainService, TrainServiceStop
 
 
-def get_service_colour(colour_directory: str, slug: str) -> Optional[tuple[str, str]]:
-    service_file = colours_dir / colour_directory / slug
-    if os.path.exists(service_file):
-        with open(service_file, "r") as f:
-            colours = f.readlines()
-        return (colours[0], colours[1])
+def get_service_colour(slug: str) -> Optional[tuple[str, str]]:
+    service_json = get_json_from_api(f"bus/service/{slug}")
+    if service_json is not None:
+        return (service_json["fg"], service_json["bg"])
     return None
 
 
@@ -83,11 +80,11 @@ def get_segment_colour(
     get_search_string: Callable[[T], str],
     driver,
 ) -> tuple[str, str]:
-    possible_colours = get_service_colour(colour_directory, route_identifier)
+    possible_colours = get_service_colour(route_identifier)
     if possible_colours is not None:
         return possible_colours
-    bg_colour = None
     fg_colour = None
+    bg_colour = None
     try:
         current_stop_index = 0
         while bg_colour is None and fg_colour is None:
@@ -103,18 +100,18 @@ def get_segment_colour(
                 if route_box is not None:
                     style = route_box.get_attribute("style")
                     style_dict = get_style_dict(style)
-                    bg_colour = get_colour(style_dict, "background-color", "#808080")
                     fg_colour = get_colour(style_dict, "color", "#ffffff")
+                    bg_colour = get_colour(style_dict, "background-color", "#808080")
                     break
             except:
                 pass
             current_stop_index = current_stop_index + 1
     except:
         print("Could not get colour!")
-        fg_colour = input("Background colour: ")
-        bg_colour = input("Foreground colour: ")
-    record_colours(colour_directory, route_identifier, bg_colour, fg_colour)
-    return (bg_colour, fg_colour)
+        fg_colour = input("Foreground colour: ")
+        bg_colour = input("Background colour: ")
+    # record_colours(colour_directory, route_identifier, fg_colour, bg_colour)
+    return (fg_colour, bg_colour)
 
 
 def get_bus_stop_search_string(stop: BusTripStop) -> str:
@@ -128,15 +125,4 @@ def get_railway_station_search_string(station: TrainServiceStop) -> str:
 def get_bus_trip_colour(trip: BusTrip, driver) -> tuple[str, str]:
     return get_segment_colour(
         "bus", trip.number, trip.slug, trip.stops, get_bus_stop_search_string, driver
-    )
-
-
-def get_train_trip_colour(trip: TrainService, driver) -> tuple[str, str]:
-    return get_segment_colour(
-        "train",
-        trip.operator,
-        trip.operator.lower().replace(" ", "-"),
-        trip.stops,
-        get_railway_station_search_string,
-        driver,
     )
